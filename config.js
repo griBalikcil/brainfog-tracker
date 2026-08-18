8e1f5546a296446a6b1485eb071976e57c480980
griBalikcil <maliclik@gmail.com>
2026-08-18 08:44:26 +0300

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // IMPORTANT: use the public/publishable key, NEVER service_role.
 window.APP_CONFIG = {
   SUPABASE_URL: "sb_publishable_LcdRKRJkaGKnUVgFGzAMdw_B6dvMyMX",
-  SUPABASE_PUBLISHABLE_KEY: "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_LcdRKRJkaGKnUVgFGzAMdw_B6dvMyMX",
   SITE_URL: "https://tsbhgvwwfqkguoxmwjrw.supabase.co
 "
 };
